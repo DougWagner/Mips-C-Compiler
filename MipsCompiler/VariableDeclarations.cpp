17e43c80379ece87a04725388e3b0703ec9da56b
jeugenio93 <jeugenio93@gmail.com>
2015-12-03 20:05:39 -0800

    Made changes to the declarations part of the compiler

--- MipsCompiler/VariableDeclarations.cpp
+++ MipsCompiler/VariableDeclarations.cpp
@@ -1,19 +1,25 @@
 #include "VariableDeclarations.h"
 
-IntDeclaration::IntDeclaration(std::string vName, std::string vType)
+IntDeclaration::IntDeclaration(std::string vName, std::string vValue)
 {
 	varName = vName;
-	varType = vType;
+	varValue = vValue;
 
 }
-IntDeclaration::IntDeclaration(std::string vName, std::string vType,
-	std::string vValue)
+IntDeclaration::IntDeclaration(std::string vName)
 {
 	varName = vName;
-	varType = vType;
-	varValue = vValue;
+	varValue = "0";
 }
-void IntDeclaration::convertToMips()
+std::string IntDeclaration::convertToMips()
 {
 	std::string converted = ("%s \t\t .word %s", varName, varValue);
+	return converted;
+}
+std::string initVar(std::string name, std::string value)
+{
+	std::string s1 = ("li $t0, %s\n", value);
+	std::string s2 = ("sw $t0, %s\n", name);
+	std::string converted = s1 + s2;
+	return converted;
 }
--- MipsCompiler/VariableDeclarations.h
+++ MipsCompiler/VariableDeclarations.h
@@ -7,17 +7,38 @@ public:
 	VariableDeclartation();
 	virtual std::string getVarName() = 0;
 	virtual std::string getVarValue() = 0;
-	virtual void convertToMips() = 0;
+	virtual std::string convertToMips() = 0;
 };
 
 class IntDeclaration : private virtual VariableDeclartation
 {
 private:
-	std::string varName, varType, varValue;
+	std::string varName, varValue;
 public:
-	IntDeclaration(std::string vName, std::string vType);
-	IntDeclaration(std::string vName, std::string vType, std::string vValue);
+	/*
+		Standard initializer for var declarations.
+		It takes two parameters.
+		IntDeclaration(string Name, string value)
+	*/
+	IntDeclaration(std::string vName, std::string vValue);
+	/*
+		This is an override incase no value is given.
+		It will take the name and set the value to zero.
+		Takes one parameter.
+		IntDeclaration(string name)
+	*/
+	IntDeclaration(std::string vName);
 	std::string getVarName() { return varName; }
-	void convertToMips();
-
+	/*
+		This converts C code to mips code 
+		and adds it to the .data portion of
+		the mips code.
+	*/
+	std::string convertToMips();
+	/*
+		This is for when a variable is later
+		initialized. takes in two strings.
+		initVar(string variableName, string value)
+	*/
+	std::string initVar(std::string, std::string);
 };
