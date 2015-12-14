0742ba943857528975b70d6a2b69ab651d35a133
jeugenio93 <jeugenio93@gmail.com>
2015-12-14 13:31:50 -0800

    FUCKEN FIXED IT AGAIN!!!!!! YAH!!!!

--- MipsCompiler/Arithmetic.cpp
+++ MipsCompiler/Arithmetic.cpp
@@ -21,8 +21,8 @@ std::string Addition::arithmeticOp()
 	//std::string s2 = ("lw $t0, %s($zero)\n", name2);
 	//std::string s3 = "add $t2, $t0, $t1\n";
 	//std::string s4 = ("sw $t2, %s\n", name3);
-	ss << "lw $t0, " << name1 << "($zero)\nlw $t0, " <<
-		name2 << "($zero)\nadd $t2, $t0, $t1\nsw $t2, " <<
+	ss << "\t\tlw $t0, " << name1 << "($zero)\n\t\tlw $t0, " <<
+		name2 << "($zero)\n\t\tadd $t2, $t0, $t1\nsw $t2, " <<
 		name3;
 	//std::string converted = s1 + s2 + s3 + s4;
 	return ss.str();
