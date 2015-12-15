76015dd5dce977758b7aab8c285a7a688c333a13
jeugenio93 <jeugenio93@gmail.com>
2015-12-14 19:26:54 -0800

    made changes to arithmetic classes

--- MipsCompiler/Arithmetic.cpp
+++ MipsCompiler/Arithmetic.cpp
@@ -6,6 +6,25 @@ Arithmetic::Arithmetic(std::string n1,
 	name1 = n1;
 	name2 = n2;
 	name3 = n3;
+	for (int i = 0; i < name1.length(); i++)
+	{
+		if (!isdigit(name1[i]))
+		{
+			checkName1 = 0;
+			break;
+		}
+		checkName1 = 1;
+	}
+	for (int i = 0; i < name2.length(); i++)
+	{
+		if (!isdigit(name2[i]))
+		{
+			checkName2 = 0;
+			break;
+		}
+		checkName2 = 1;
+	}
+	arithType = checkName1 + checkName2;
 }
 Addition::Addition(std::string n1,
 	std::string n2, std::string n3) :
@@ -16,12 +35,32 @@ Addition::Addition(std::string n1,
 std::string Addition::arithmeticOp()
 {
 	// This Assumes that the two variables being added are declared!
+
+
 	std::stringstream ss;
-	ss << "\t\tlw $t0, " << name1 << "($zero)\n\t\tlw $t1, " <<
-		name2 << "($zero)\n\t\tadd $t2, $t0, $t1" << std::endl
-		<<"\t\tsw $t2, " << name3 << std::endl;
+	if (arithType == 0)
+	{
+		ss << "\t\tlw $t0, " << name1 << "($zero)\n\t\tlw $t1, " <<
+			name2 << "($zero)\n\t\tadd $t2, $t0, $t1" << std::endl
+			<< "\t\tsw $t2, " << name3 << std::endl;
+	}
+	if (arithType == 1)
+	{
+		ss << "\t\tlw $t0, " << name1 << "($zero)" << 
+			  "\n\t\taddi $t2, $t0, " << name2 << std::endl << 
+			  "\t\tsw $t2, " << name3 << std::endl;
+	}
+	if (arithType == 2)
+	{
+		ss << "\t\tli $t0, " << name1 << std::endl <<
+			"\t\tli $t1, " << name2 << std::endl <<
+			"\t\tadd $t2, $t0, $t1" << std::endl <<
+			"\t\tsw $t2, " << name3 << std::endl;
+	}
 	return ss.str();
 }
+
+
 Subtraction::Subtraction(std::string n1, 
 	std::string n2, std::string n3) :
 	Arithmetic(n1, n2, n3)
@@ -32,9 +71,31 @@ std::string Subtraction::arithmeticOp()
 {
 	// This Assumes that the two variables being added are declared!
 	std::stringstream ss;
+	/*
 	ss << "\t\tlw $t0, " << name1 << "($zero)\n\t\tlw $t1, " <<
 		name2 << "($zero)\n\t\tsub $t2, $t0, $t1" << std::endl
 		<< "\t\tsw $t2, " << name3 << std::endl;
+	*/
+	if (arithType == 0)
+	{
+		ss <<  "\t\tlw $t0, " << name1 << 
+			   "($zero)\n\t\tlw $t1, " << name2 << 
+			   "($zero)\n\t\tsub $t2, $t0, $t1" << std::endl << 
+			   "\t\tsw $t2, " << name3 << std::endl;
+	}
+	if (arithType == 1)
+	{
+		ss << "\t\tlw $t0, " << name1 << "($zero)" <<
+			"\n\t\tsubi $t2, $t0, " << name2 << std::endl <<
+			"\t\tsw $t2, " << name3 << std::endl;
+	}
+	if (arithType == 2)
+	{
+		ss << "\t\tli $t0, " << name1 << std::endl <<
+			"\t\tli $t1, " << name2 << std::endl <<
+			"\t\tsub $t2, $t0, $t1" << std::endl <<
+			"\t\tsw $t2, " << name3 << std::endl;
+	}
 	return ss.str();
 }
 Multiplication::Multiplication(std::string n1,
@@ -47,10 +108,31 @@ std::string Multiplication::arithmeticOp()
 {
 	// This Assumes that the two variables being added are declared!
 	std::stringstream ss;
-	ss << "\t\tlw $t0, " << name1 << "($zero)\n\t\tlw $t1, " <<
-		name2 << "($zero)\n\t\tmult $t0, $t1" << std::endl
-		<< "\t\tmflo $t2\n"
-		<< "\t\tsw $t2, " << name3 << std::endl;
+	if (arithType == 0)
+	{
+		ss << "\t\tlw $t0, " << name1 << std::endl <<
+			"($zero)\n\t\tlw $t1, " << name2 <<
+			"($zero)\n\t\tmult $t0, $t1" << std::endl <<
+			"\t\tmflo $t2\n" <<
+			"\t\tsw $t2, " << name3 << std::endl;
+	}
+	else if (arithType == 1)
+	{
+		ss << "\t\tlw $t0, " << name1 << "($zero)" << std::endl <<
+			"\t\tli $ti, " << name2 << std::endl <<
+			"\t\tmult $t0, $t1\n" <<
+			"\t\tmflo $t2\n" <<
+			"\t\tsw $t2, " << name3 << std::endl;
+	}
+	else if (arithType == 2)
+	{
+		ss << "\t\tli $t0, " << name1 << std::endl <<
+			"\t\tli $ti, " << name2 << std::endl <<
+			"\t\tmult $t0, $t1\n" <<
+			"\t\tmflo $t2\n" <<
+			"\t\tsw $t2, " << name3 << std::endl;
+	}
+	
 	return ss.str();
 }
 Division::Division(std::string n1,
@@ -63,10 +145,37 @@ std::string Division::arithmeticOp()
 {
 	// This Assumes that the two variables being added are declared!
 	std::stringstream ss;
+	/*
 	ss << "\t\tlw $t0, " << name1 << "($zero)\n\t\tlw $t1, " <<
 		name2 << "($zero)\n\t\tdiv $t0, $t1" << std::endl
 		<<	"\t\tmflo $t2\n"
 		<< "\t\tsw $t2, " << name3 << std::endl;
+	*/
+	if (arithType == 0)
+	{
+		ss << "\t\tlw $t0, " << name1 << std::endl <<
+			"($zero)\n\t\tlw $t1, " << name2 <<
+			"($zero)\n\t\tdiv $t0, $t1" << std::endl <<
+			"\t\tmflo $t2\n" <<
+			"\t\tsw $t2, " << name3 << std::endl;
+	}
+	else if (arithType == 1)
+	{
+		ss << "\t\tlw $t0, " << name1 << "($zero)" << std::endl <<
+			"\t\tli $ti, " << name2 << std::endl <<
+			"\t\tdiv $t0, $t1\n" <<
+			"\t\tmflo $t2\n" <<
+			"\t\tsw $t2, " << name3 << std::endl;
+	}
+	else if (arithType == 2)
+	{
+		ss << "\t\tli $t0, " << name1 << std::endl <<
+			"\t\tli $ti, " << name2 << std::endl <<
+			"\t\tdiv $t0, $t1\n" <<
+			"\t\tmflo $t2\n" <<
+			"\t\tsw $t2, " << name3 << std::endl;
+	}
+
 	return ss.str();
 }
 
--- MipsCompiler/Arithmetic.h
+++ MipsCompiler/Arithmetic.h
@@ -10,16 +10,19 @@
 #include "Tokenizer.h"
 #include <sstream>
 
+
 class Arithmetic
 {
 
 protected:
 	std::string name1, name2, name3;
+	int arithType = 0;
+	int checkName1 = 0;
+	int checkName2 = 0;
 public:
 	Arithmetic() {}
 	Arithmetic(std::string, std::string, std::string);
 	virtual std::string arithmeticOp() = 0;
-	//virtual std::string arithmeticOpWithLit() = 0;
 };
 
 class Addition : public virtual  Arithmetic
@@ -28,7 +31,6 @@ public:
 	Addition() {}
 	Addition(std::string, std::string, std::string);
 	std::string arithmeticOp();
-	//std::string arithmeticOpWithLit()
 };
 							   
 class Subtraction : public virtual Arithmetic
@@ -37,7 +39,6 @@ public:
 	Subtraction() {}
 	Subtraction(std::string, std::string, std::string);
 	std::string arithmeticOp();
-	//std::string arithmeticOpWithLit()
 };
 
 class Multiplication : public virtual Arithmetic
@@ -46,7 +47,6 @@ public:
 	Multiplication() {}
 	Multiplication(std::string, std::string, std::string);
 	std::string arithmeticOp();
-	//std::string arithmeticOpWithLit()
 };
 
 class Division : public virtual Arithmetic
@@ -55,5 +55,4 @@ public:
 	Division() {}
 	Division(std::string, std::string, std::string);
 	std::string arithmeticOp();
-	//std::string arithmeticOpWithLit()
 };
